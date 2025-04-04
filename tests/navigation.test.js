082e3059dd4f7c20c56c05a505e4db2d6b12903e
Bagzy3 <gabin.paolino@hotmail.com>
2025-04-04 12:44:50 +0200

    Message de commit

---
+++ tests/navigation.test.js
@@ -0,0 +1,49 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('US2 - Navigation & Consultation des Produits', () => {
+  test.beforeEach(async ({ page }) => {
+    // Aller sur la page de login
+    await page.goto('https://www.saucedemo.com/');
+    // Remplir les champs et se connecter
+    await page.fill('#user-name', 'standard_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+    // Vérifie qu'on est bien connecté
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  });
+
+  test('TC06 - Tous les produits ont une image, un nom et un prix', async ({ page }, testInfo) => {
+    // Attendre que les produits apparaissent dans la page
+    const productsSelector = '[data-test="inventory-item"]';
+    await page.waitForSelector(productsSelector, { timeout: 5000 });
+
+    // Localiser tous les éléments avec le sélecteur 'inventory-item'
+    const products = page.locator(productsSelector);
+
+    // Vérifier qu'il y a au moins un produit
+    const count = await products.count();
+    if (count === 0) {
+      throw new Error('Il n\'y a aucun produit sur la page');
+    }
+
+    console.log(`Nombre de produits: ${count}`);
+
+    // Vérifier que chaque produit a une image, un nom et un prix
+    for (let i = 0; i < count; i++) {
+      const product = products.nth(i);
+      const img = product.locator('img');
+      const name = product.locator('.inventory_item_name');
+      const price = product.locator('.inventory_item_price');
+
+      // Vérifier que l'image est visible
+      await expect(img).toBeVisible();
+
+      // Vérifier que le nom du produit est visible
+      await expect(name).toBeVisible();
+
+      // Vérifier que le prix du produit est visible
+      await expect(price).toBeVisible();
+      
+    }
+  });
+});
