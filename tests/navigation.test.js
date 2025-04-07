c49b59cd0c674268463ec1d78990396b8de72af8
Bagzy3 <gabin.paolino@hotmail.com>
2025-04-07 11:25:21 +0200

    Premier commit de mon projet

--- tests/navigation.test.js
+++ tests/navigation.test.js
@@ -13,15 +13,12 @@ test.describe('US2 - Navigation & Consultation des Produits', () => {
   });
 
   test('TC06 - Tous les produits ont une image, un nom et un prix', async ({ page }, testInfo) => {
-    // Attendre que les produits apparaissent dans la page
+    
     const productsSelector = '[data-test="inventory-item"]';
     await page.waitForSelector(productsSelector, { timeout: 5000 });
+    const products = page.locator(productsSelector); // Localiser tous les éléments avec le sélecteur 'inventory-item'
 
-    // Localiser tous les éléments avec le sélecteur 'inventory-item'
-    const products = page.locator(productsSelector);
-
-    // Vérifier qu'il y a au moins un produit
-    const count = await products.count();
+    const count = await products.count();// Vérifier qu'il y a au moins un produit
     if (count === 0) {
       throw new Error('Il n\'y a aucun produit sur la page');
     }
@@ -46,4 +43,31 @@ test.describe('US2 - Navigation & Consultation des Produits', () => {
       
     }
   });
-});
+
+  test('TC07 - Tri des produits par nom et prix', async ({ page }, testInfo) => {
+    
+    // Test de tri dans l'ordre A-Z
+    await page.selectOption('[data-test="product-sort-container"]', 'az'); 
+    const ordre_az = await page.locator('.inventory_item_name').allInnerTexts();
+    expect(ordre_az).toEqual([...ordre_az].sort());
+
+    // Test de tri dans l'ordre Z-A
+    await page.selectOption('[data-test="product-sort-container"]', 'za'); 
+    const ordre_za = await page.locator('.inventory_item_name').allInnerTexts();
+    expect(ordre_za).toEqual([...ordre_za].sort().reverse());
+
+    // Test de tri prix dans l'ordre croissant
+    await page.selectOption('[data-test="product-sort-container"]', 'lohi');  
+    const prixTextCr = await page.locator('.inventory_item_price').allInnerTexts();
+    const prixCroissants = prixTextCr.map(p => parseFloat(p.replace(/[^0-9,\.]/g, '').replace(',', '.'))); 
+    expect(prixCroissants).toEqual([...prixCroissants].sort((a, b) => a - b)); 
+
+    // Test de tri prix dans l'ordre décroissant
+    await page.selectOption('[data-test="product-sort-container"]', 'hilo');  
+    const prixTextDecr = await page.locator('.inventory_item_price').allInnerTexts();
+    const prixDecroissants = prixTextDecr.map(p => parseFloat(p.replace(/[^0-9,\.]/g, '').replace(',', '.'))); 
+    expect(prixDecroissants).toEqual([...prixDecroissants].sort((a, b) => b - a));
+    
+  });
+  
+});  
