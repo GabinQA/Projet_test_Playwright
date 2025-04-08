23bd6f71f9b7e0883475d26cfc891da601bb806e
Bagzy3 <gabin.paolino@hotmail.com>
2025-04-08 13:36:01 +0200

    Commit

--- tests/panier.test.js
+++ tests/panier.test.js
@@ -21,7 +21,7 @@ test('TC09 - Ajouter un produit au panier', async ({ page }) => {
     await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1'); // vérifier qu'il affiche '1'
 
     // ajouter un autre produit et vérfifier que le badge affiche "2"
-    await page.click('#add-to-cart-sauce-labs-fleece-jacket')
+    await page.click('#add-to-cart-sauce-labs-fleece-jacket') // Cliquer sur add to cart du produit sauce labs fleece jacket
     await expect(page.locator('[data-test="remove-sauce-labs-fleece-jacket"]')).toBeVisible(); // vérfifie que le bouton remove est apparu à la place du bouton add to cart 
     await expect(page.locator('[data-test="remove-sauce-labs-fleece-jacket"]')).toBeEnabled(); // vérifie que le bouton remove est cliquable 
     await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('2'); // vérifier qu'il affiche '2'
@@ -30,5 +30,20 @@ test('TC09 - Ajouter un produit au panier', async ({ page }) => {
 
 test('TC10 - Supprimer un produit du panier depuis la page panier', async ({ page }) => {
 
+    await page.click('#add-to-cart-sauce-labs-backpack') // Cliquer sur add to cart du produit sauce labs backpack
+    await page.click('#add-to-cart-sauce-labs-fleece-jacket') // Cliquer sur add to cart du produit sauce labs fleece jacket
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('2'); // vérifier que le badge du panier affiche '2'
+    await page.click('[data-test="shopping-cart-link"]'); // Cliquer sur le panier
+    await expect(page).toHaveURL(/.*cart\.html/); // vérifier que cart.html est dans l'URL de la page = on est sur la page panier
 
+    await expect(page.locator('div[data-test="inventory-item"] >> text="Sauce Labs Backpack"')).toBeVisible();  // Vérifier que les produits sont dans le panier
+    await expect(page.locator('div[data-test="inventory-item"] >> text="Sauce Labs Fleece Jacket"')).toBeVisible(); // Vérifier que les produits sont dans le panier
+
+    await page.locator('button[data-test="remove-sauce-labs-backpack"]').click(); // Cliquer sur le bouton remove sous le produit sauce labs backpack
+    await expect(page.locator('div[data-test="inventory-item"] >> text="Sauce Labs Backpack"')).not.toBeVisible(); // vérifie qu'il n'est plus visible après suppression
+    await expect(page.locator('div[data-test="inventory-item"] >> text="Sauce Labs Fleece Jacket"')).toBeVisible(); // vérifie que l'autre produit reste visible
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1'); // vérifier que le badge du panier affiche 1.
 });
+
+
+
